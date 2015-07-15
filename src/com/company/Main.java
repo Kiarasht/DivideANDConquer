a854bbe9d7ff772fcb3732b8e75c8b7e87e9f75b
restart32 <kiarashbikar@gmail.com>
2015-07-15 01:14:42 -0700

    Added comments, will clean and add more functions later.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,35 +5,35 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        final long startTime = System.nanoTime();
-        final int start = (int) ((Math.random() * 10) + 1);
-        final int start2 = (int) ((Math.random() * 10) + 1);
-        //final int start = 1000000000;
-        //final int start2 = 1000000000;
-        int[] a = new int[start];
-        int[] b = new int[start2];
+        final long startTime = System.nanoTime();                           // Start run time
+        final int start = (int) ((Math.random() * 10) + 1);                 // Random array, 1 to 10
+        final int start2 = (int) ((Math.random() * 10) + 1);                // Random array, 1 to 10
 
-        for (int i = 0; i < a.length; ++i) {
+        int[] a = new int[start];                                           // Set up array by the random value
+        int[] b = new int[start2];                                          // Create another one
+
+        for (int i = 0; i < a.length; ++i) {                                // Give the first array random values
             a[i] = (int) ((Math.random() * 10) + 1);
         }
-        for (int i = 0; i < b.length; ++i) {
+        for (int i = 0; i < b.length; ++i) {                                // Give the second array random values
             b[i] = (int) ((Math.random() * 10) + 1);
         }
 
-        int[] c = new int[a.length + b.length];
-        System.out.println(Arrays.toString(a));
+        int[] c = new int[a.length + b.length];                             // Merge sorted array maximum size given
+
+        System.out.println(Arrays.toString(a));                             // Print arrays for clarity
         System.out.println(Arrays.toString(b));
         System.out.println();
-        mergesort(a);
-        mergesort(b);
-        merge(c, a, b);
-        c = removeduplicate(c);
-        System.out.println(Arrays.toString(c));
-        final long duration = System.nanoTime() - startTime;
+        mergesort(a);                                                       // Sort the first array
+        mergesort(b);                                                       // Sort the second one
+        merge(c, a, b);                                                     // Merge both arrays in c
+        c = removeduplicate(c);                                             // Remove any duplicates
+        System.out.println(Arrays.toString(c));                             // Final value of array c
+        final long duration = System.nanoTime() - startTime;                // Final run time if calculated and printed
         System.out.println("Time: " + duration / 1000000 + " millisecond.");
     }
 
-    private static int[] removeduplicate(int[] c) {
+    private static int[] removeduplicate(int[] c) {                         // Removes duplicates from passed in arrays
         int[] temp = new int[c.length];
         int loop = 1;
         temp[0] = c[0];
@@ -47,34 +47,34 @@ public class Main {
         return temp2;
     }
 
-    static void mergesort(int[] a) {
-        if (a.length > 1) {
+    static void mergesort(int[] a) {                                        // Divided a given array
+        if (a.length > 1) {                                                 // Recursively calls itself till size of 1
             int[] u = Arrays.copyOfRange(a, 0, a.length / 2);
             int[] v = Arrays.copyOfRange(a, a.length / 2, a.length);
             mergesort(u);
             mergesort(v);
-            merge(a, u, v);
+            merge(a, u, v);                                                 // Starts ordering them
         }
     }
 
     static void merge(int[] a, int[] u, int[] v) {
         int i, j, k;
         i = j = k = 0;
-        while (i < u.length + v.length) {
-            if (j < u.length && k < v.length) {
-                if (u[j] < v[k]) {
+        while (i < u.length + v.length) {                                   // If a array has space
+            if (j < u.length && k < v.length) {                             // And both array have elements left
+                if (u[j] < v[k]) {                                          // First array has the smaller element
                     a[i++] = u[j++];
-                } else {
+                } else {                                                    // Second array has the smaller element
                     a[i++] = v[k++];
                 }
-            } else {
-                if (j >= u.length) {
-                    while (k < v.length) {
+            } else {                                                        // One of the arrays is exhausted
+                if (j >= u.length) {                                        // Array u is exhausted
+                    while (k < v.length) {                                  // Copy remaining v elements
                         a[i++] = v[k++];
                     }
                 }
-                if (k >= v.length) {
-                    while (j < u.length) {
+                if (k >= v.length) {                                        // Array v is exhausted
+                    while (j < u.length) {                                  // Copy remaining u elements
                         a[i++] = u[j++];
                     }
                 }
